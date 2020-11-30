ec860f3230067f32b980abbb480a26807f789b91
John Sinclair <sinclairjohn72@gmail.com>
2020-11-30 11:00:49 +0000

    cleaned up few files

--- src/api/followersCall.js
+++ src/api/followersCall.js
@@ -3,16 +3,8 @@ async function getFollowers(input) {
         `https://api.github.com/users/${input}/followers`
     );
     const follwersJSON = await followers.json();
-    // console.log(follwersJSON);
-    // const following = await fetch(
-    //     `https://api.github.com/users/${input}/following`
-    // );
-    // const followingJSON = await following.json();
-    // console.log(followingJSON);
 
     return follwersJSON;
 }
 
 export default getFollowers;
-
-//todo create two way link between friends, differeing colours
--- src/api/repoCall.js
+++ src/api/repoCall.js
@@ -1,14 +1,4 @@
-//const axios = require("axios");
-
 async function getRepos(input) {
-    // axios
-    //     .get(`https://api.github.com/users/${input}/repos`)
-    //     .then(async function (res) {
-    //         const repoJSON = await res.data.json();
-    //         console.log(repoJSON);
-    //         // return repoJSON;
-    //     });
-
     const data = await fetch(`https://api.github.com/users/${input}/repos`);
     const dataJSON = await data.json();
     //console.log(dataJSON);
